8cc17700260a6bf7a73aa5808682be8b486c3a3d
lgentile06 <egentile@umich.edu>
2016-10-09 15:11:41 -0400

    first commit heeheh

--- ReminderApp/ViewController.swift
+++ ReminderApp/ViewController.swift
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: properties
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self;
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    // MARK: actions
 
 }
 
